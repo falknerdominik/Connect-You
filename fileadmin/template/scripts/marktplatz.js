13dd1623dbf021210a0942a04a6b9de4da618bb3
VALE <vale1_@gmx.at>
2013-10-10 10:01:52 +0200

    / ‘.idea/workspace.xml’ / ‘fileadmin/template/scripts/marktplatz.js’

--- fileadmin/template/scripts/marktplatz.js
+++ fileadmin/template/scripts/marktplatz.js
@@ -6,6 +6,6 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$( ".projekt" ).click(function() {
+$( " .projekt " ).click(function() {
     alert( "Handler for .click() called." );
 });
