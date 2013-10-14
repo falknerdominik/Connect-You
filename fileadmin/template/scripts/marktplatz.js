8e1b8243131f01cf1839ecee32481044d9397823
VALE <vale1_@gmx.at>
2013-10-14 12:43:02 +0200

    / ‘.idea/workspace.xml’ / ‘fileadmin/template/scripts/marktplatz.js’

--- fileadmin/template/scripts/marktplatz.js
+++ fileadmin/template/scripts/marktplatz.js
@@ -20,5 +20,5 @@ $( document ).ready(function() {
 function scrollToDetails(){
 
         //$('body').scrollTo( {top:'30%', left:'0px'}, 800, {easing:'easeInBounce'} );
-        scrollTo(0,700);
+        scrollTo(0,700, {easing:'easeInBounce'});
 }
