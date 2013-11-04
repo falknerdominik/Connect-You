7ffcd36bad975796cf1ebe2419e3b25dbdb7db28
Dominik Falkner <falkner.dominik@gmail.com>
2013-11-04 13:31:56 +0100

    / ‘typo3conf/ext/cy_marketplace/Classes/Domain/Model/User.php’

--- typo3conf/ext/cy_marketplace/Classes/Domain/Model/User.php
+++ typo3conf/ext/cy_marketplace/Classes/Domain/Model/User.php
@@ -32,7 +32,8 @@ namespace TYPO3\CyMarketplace\Domain\Model;
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class User extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+ 
+class User extends \TYPO3\CMS\Extbase\Domain\Model\FrontendUser  {
 
 	/**
 	 * timeset
