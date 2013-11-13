1bbe03219e287b9986cdf44cd9409411c2a4166d
Dominik Falkner <falkner.dominik@gmail.com>
2013-11-13 09:24:17 +0100

    / ‘fileadmin/template/scripts/marktplatz.js’

--- fileadmin/template/scripts/marktplatz.js
+++ fileadmin/template/scripts/marktplatz.js
@@ -1,12 +1,15 @@
 /**
  * Created with JetBrains PhpStorm.
- * User: VALE
+ * User: VALE, Falkner
  * Date: 10.10.13
  * Time: 09:03
  * To change this template use File | Settings | File Templates.
  */
 
+ // READY
 $( document ).ready(function() {
+
+	// bind functions to elements
 	$( "#projects li" ).click(scrollToDetails);
 });
 
