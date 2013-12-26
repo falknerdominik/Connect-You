165bf980f719e732ca1f4e8b39d00f75631c4793
Dominik Falkner <falkner.dominik@gmail.com>
2013-12-26 14:49:56 +0100

    / ‘.idea/workspace.xml’ / ‘typo3conf/ext/connectyou/Classes/Domain/Model/User.php’

--- typo3conf/ext/connectyou/Classes/Domain/Model/User.php
+++ typo3conf/ext/connectyou/Classes/Domain/Model/User.php
@@ -32,7 +32,7 @@ namespace TYPO3\Connectyou\Domain\Model;
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class User extends TYPO3\CMS\Extbase\Domain\Model\FrontendUser {
+class User extends \TYPO3\CMS\Extbase\Domain\Model\FrontendUser {
 
 }
 ?>
