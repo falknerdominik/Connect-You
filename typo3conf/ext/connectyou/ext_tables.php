c1754df5367cc29d18080328382af6f3bcb1c2ba
Dominik Falkner <falkner.dominik@gmail.com>
2013-12-30 22:30:12 +0100

    / ‘.idea/workspace.xml’ / ‘typo3conf/ext/connectyou/ext_tables.php’

--- typo3conf/ext/connectyou/ext_tables.php
+++ typo3conf/ext/connectyou/ext_tables.php
@@ -139,7 +139,7 @@ $tmp_connectyou_columns['project'] = array(
 
 t3lib_extMgm::addTCAcolumns('fe_users',$tmp_connectyou_columns);
 
-$TCA['fe_users']['columns'][$TCA['fe_users']['ctrl']['type']]['config']['items'][] = array('LLL:EXT:connectyou/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.Tx_Connectyou_User','Tx_Connectyou_User');
+$TCA['fe_users']['columns'][$TCA['fe_users']['ctrl']['type']]['config']['items'][] = array('LLL:EXT:connectyou/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.Tx_Extbase_Domain_Model_FrontendUser','Tx_Extbase_Domain_Model_FrontendUser');
 
 $TCA['fe_users']['types']['Tx_Extbase_Domain_Model_FrontendUser']['showitem'] = $TCA['fe_users']['types']['1']['showitem'];
 $TCA['fe_users']['types']['Tx_Extbase_Domain_Model_FrontendUser']['showitem'] .= ',--div--;LLL:EXT:connectyou/Resources/Private/Language/locallang_db.xlf:Tx_Extbase_Domain_Model_FrontendUser,';
