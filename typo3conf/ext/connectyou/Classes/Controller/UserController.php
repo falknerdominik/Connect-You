478f16d92202383a712659b0337c4c2958376a6b
Dominik Falkner <falkner.dominik@gmail.com>
2014-01-02 23:18:41 +0100

    / ‘.idea/workspace.xml’ / ‘typo3conf/ext/connectyou/Classes/Controller/UserController.php’

--- typo3conf/ext/connectyou/Classes/Controller/UserController.php
+++ typo3conf/ext/connectyou/Classes/Controller/UserController.php
@@ -58,7 +58,7 @@ class UserController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 	public function listAction() {
 		$users = $this->userRepository->findAll();
         $projects = $this->projectRepository->findAll();
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($this->projectRepository);
+        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($this->userRepository);
 		$this->view->assign('users', $users);
         $this->view->assign('projects', $projects);
 	}
