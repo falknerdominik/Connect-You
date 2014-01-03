b9ed6445214eea8df9387c7a434dae787bda9dc3
Dominik Falkner <falkner.dominik@gmail.com>
2014-01-03 04:32:00 +0100

    / ‘.idea/workspace.xml’ / ‘typo3conf/ext/connectyou/Classes/Controller/ProjectController.php’

--- typo3conf/ext/connectyou/Classes/Controller/ProjectController.php
+++ typo3conf/ext/connectyou/Classes/Controller/ProjectController.php
@@ -119,9 +119,9 @@ class ProjectController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
         #debug
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($formData);
         \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($project);
-        #$this->projectRepository->update($project);
+        $this->projectRepository->update($project);
 		#$this->flashMessageContainer->add('Your Project was updated.');
-		#$this->redirect('list');
+		$this->redirect('list');
 	}
 
 	/**
