48033f0f29cb2899cb1a12f2d8086db753ac780d
Valentin Dorfer <vale1_@gmx.at>
2014-01-16 10:25:15 +0100

    / ‘.idea/workspace.xml’ / ‘fileadmin/template/scripts/pinboard.js’

--- fileadmin/template/scripts/pinboard.js
+++ fileadmin/template/scripts/pinboard.js
@@ -3,12 +3,13 @@
  */
 
 
-var widgets = array();
+
 
 // widgets[0] = "<div class='deletewidget panel panel-primary col-md-3 col-md-offset-1' ><div class='panel-heading' > Kalender <i class='remove glyphicon glyphicon-remove'></i> </div ><div class='panel-body'>";
 
 
 window.onload = function () {
+    var widgets = array();
     $("#move").click(function () {
         $("#widgetmenu").animate({
             left: "0em"
