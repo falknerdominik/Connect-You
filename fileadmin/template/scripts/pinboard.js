7208c78da2fc97539983cc04af3aeeba2cde277a
Valentin Dorfer <vale1_@gmx.at>
2014-01-16 10:23:41 +0100

    / ‘.idea/workspace.xml’ / ‘fileadmin/template/scripts/pinboard.js’

--- fileadmin/template/scripts/pinboard.js
+++ fileadmin/template/scripts/pinboard.js
@@ -2,6 +2,12 @@
  * Created by Valentin on 16.01.14.
  */
 
+
+var widgets = array();
+widgets[0] = "<div class='deletewidget panel panel-primary col-md-3 col-md-offset-1' ><div class='panel-heading' > Kalender <i class='remove glyphicon glyphicon-remove'></i> </div ><div class='panel-body'>"
+
+
+
 window.onload = function () {
     $("#move").click(function () {
         $("#widgetmenu").animate({
@@ -16,7 +22,7 @@ window.onload = function () {
 
     $(function () {
         $("li").click(function () {
-            $("#content").append("<div class='deletewidget panel panel-primary col-md-3 col-md-offset-1' ><div class='panel-heading' > Kalender <i class='remove glyphicon glyphicon-remove'></i> </div ><div class='panel-body'>");
+            $("#content").append();
         });
     });
 }
